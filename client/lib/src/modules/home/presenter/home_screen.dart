1bc4b9828aa68111576f8cf341cb3bf37fb806f0
Renan Rocha <renanrochamorais@outlook.com>
2022-10-26 19:13:24 -0300

    Adição do statusBarColor

--- client/lib/src/modules/home/presenter/home_screen.dart
+++ client/lib/src/modules/home/presenter/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:sticker_swap_client/src/modules/home/presenter/home_bloc.dart';
 import 'package:sticker_swap_client/src/modules/home/presenter/widgets/home_bottom_navigator_bar.dart';
@@ -20,18 +21,23 @@ class _HomeScreenState extends ModularState<HomeScreen, HomeBloc> {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<int>(
-      initialData: 0,
-      stream: controller.getIndexTela,
-      builder: (_, snapshot) {
-        return Scaffold(
-          bottomNavigationBar: HomeBottomNavigatorBar(
-            onTap: controller.mudarTela,
-            indexTelaAtual: snapshot.data!,
-            selectedIconColor: Theme.of(context).primaryColor,
-          ),
-        );
-      }
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+          statusBarColor: Theme.of(context).primaryColor,
+        ),
+        child: StreamBuilder<int>(
+            initialData: 0,
+            stream: controller.getIndexTela,
+            builder: (_, snapshot) {
+              return Scaffold(
+                bottomNavigationBar: HomeBottomNavigatorBar(
+                  onTap: controller.mudarTela,
+                  indexTelaAtual: snapshot.data!,
+                  selectedIconColor: Theme.of(context).primaryColor,
+                ),
+              );
+            }
+        )
     );
   }
 }
