fa608e7e3ca5c327ab32ace0b43a0fc201477f3b
Guilherme <guiegle@hotmail.com>
2022-10-29 13:28:58 -0300

    fix: dotenv

--- client/lib/src/modules/login/config/api.dart
+++ client/lib/src/modules/login/config/api.dart
@@ -5,8 +5,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 Future<String?> login(email, password) async{
-  var response = await http.post(Uri.http(/*dotenv.env['API_URI']!*/ 'localhost:8081', 'api/login'), body: {'email': email, 'password': password});
-  //debugPrint(dotenv.env['API_URI']);
+  var response = await http.post(Uri.http(dotenv.env['API_URI']!, 'api/login'), body: {'email': email, 'password': password});
+  debugPrint(dotenv.env['API_URI']);
   if(response.statusCode == 201){
     var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
     return decodedResponse['token'];
@@ -16,7 +16,7 @@ Future<String?> login(email, password) async{
 }
 
 Future<String?> getRandomNumber(token) async{
-  var response = await http.get(Uri.http(/*dotenv.env['API_URI']!*/ 'localhost:8081', 'api/randomnumber'), headers: {
+  var response = await http.get(Uri.http(dotenv.env['API_URI']!, 'api/randomnumber'), headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
       'Authorization': 'Bearer $token',
