66fd1caee2ca1fedb197753f8beffe729b3526a9
Renan Rocha <renanrocha@MacBook-Air-de-Renan.local>
2022-11-05 12:09:13 -0300

    Criação de entidade filtro

---
+++ client/lib/src/modules/filter/domain/entities/filter.dart
@@ -0,0 +1,13 @@
+import 'package:sticker_swap_client/src/core/album.dart';
+import 'package:sticker_swap_client/src/utils/consts/filter_modes_utils.dart';
+
+class Filter{
+  bool filterOn = false;
+  int modeFilter = FilterModesUtils.all;
+
+  void setFilterAlbum(int modeFilte){
+    this.modeFilter = modeFilte;
+    filterOn = FilterModesUtils.all != modeFilte;
+  }
+
+}
---
+++ client/lib/src/utils/consts/filter_modes_utils.dart
@@ -0,0 +1,6 @@
+class FilterModesUtils{
+  static const int all = 0;
+  static const int onlyMissing = 1;
+  static const int onlyRepeated = 2;
+  static const int onlyObtained = 3;
+}
