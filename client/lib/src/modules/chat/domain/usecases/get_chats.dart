0f6cf0f89962b78c6cd9371cd4e72c7ee0f1d254
Renan Rocha <renanrochamorais@outlook.com>
2022-11-08 09:39:45 -0300

    Mockagem de dados do chat

--- client/lib/src/modules/chat/domain/usecases/get_chats.dart
+++ client/lib/src/modules/chat/domain/usecases/get_chats.dart
@@ -18,6 +18,12 @@ class GetChatsImpl implements IGetChats{
           name: "Vinimalvadeza Jr.",
           image: "https://i.pinimg.com/736x/71/57/6d/71576dfdc502b33c906783dc35e43de3.jpg",
           lastMessage: Message(id: 0, message: "Vc tem figurinhas pra trocar?")
+      ),
+      Chat(
+          id: 1,
+          name: "El tanque",
+          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVmSZbr3E4I5pQVoMny7KCi32RoQ2njQVxsA&usqp=CAU",
+          lastMessage: Message(id: 2, message: "Tite não me levou para copa.")
       )
     ];
   }
