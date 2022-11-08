08f7a7853ae6314600c7891caaef28f5ea4ec408
Renan Rocha <renanrochamorais@outlook.com>
2022-11-08 08:01:53 -0300

    Stream para chat

--- client/lib/src/modules/chat/presenter/chat_screen.dart
+++ client/lib/src/modules/chat/presenter/chat_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:sticker_swap_client/src/modules/chat/domain/entities/chat.dart';
 import 'package:sticker_swap_client/src/modules/chat/presenter/chat_bloc.dart';
+import 'package:sticker_swap_client/src/modules/chat/presenter/widgets/chat_tile.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -10,7 +12,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends ModularState<ChatScreen, ChatBloc> {
-  
+
   @override
   void initState() {
     super.initState();
@@ -27,7 +29,33 @@ class _ChatScreenState extends ModularState<ChatScreen, ChatBloc> {
   Widget build(BuildContext context) {
     return Expanded(
       child: Scaffold(
-        body: Text("Falta fazer a pagina do chat eim..."),
+        appBar: AppBar(
+          elevation: 0.0,
+          title: const Text("Trocar figurinhas"),
+          centerTitle: true,
+        ),
+
+        body: Column(
+          children: [
+            //Colocar search aqui
+
+            StreamBuilder<List<Chat>>(
+              stream: controller.getChatsView,
+              builder: (_, snapshot){
+                if (snapshot.hasData){
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) => ChatTile(chat: snapshot.data![index]),
+                    ),
+                  );
+                }else{
+                  return const Center(child: CircularProgressIndicator(),);
+                }
+              }
+            ),
+          ],
+        ),
 
         floatingActionButton: FloatingActionButton(
           onPressed: (){},
--- client/lib/src/modules/chat/presenter/widgets/chat_tile.dart
+++ client/lib/src/modules/chat/presenter/widgets/chat_tile.dart
@@ -15,6 +15,7 @@ class ChatTile extends StatelessWidget {
       leading: Image.network(chat.image),
       title: Text(chat.name),
       subtitle: Text(chat.lastMessage.message),
+      onTap: (){},
     );
   }
 }
