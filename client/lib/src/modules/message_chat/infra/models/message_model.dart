24dfb96df23a7c24d5dbaccf97aeff04e06f9964
Renan Rocha <renanrocha@MacBook-Air-de-Renan.local>
2022-11-29 13:24:02 -0300

    Organização da lista

--- client/lib/src/modules/message_chat/infra/models/message_model.dart
+++ client/lib/src/modules/message_chat/infra/models/message_model.dart
@@ -1,4 +1,6 @@
 import 'package:sticker_swap_client/src/modules/message_chat/domain/entities/message.dart';
+import 'package:sticker_swap_client/src/modules/message_chat/infra/models/message_place_model.dart';
+import 'package:sticker_swap_client/src/modules/message_chat/infra/models/message_simple_model.dart';
 
 class MessageModel extends Message {
   MessageModel(
@@ -18,7 +20,13 @@ class MessageModel extends Message {
 
   static List<MessageModel> listFromJson(List<dynamic> json) {
     return (json)
-        .map((e) => MessageModel.fromMap(e as Map<String, dynamic>))
-        .toList();
+        .map((e){
+          if(e['type'] == 0)
+            return MessageSimpleModel.fromMap(e as Map<String, dynamic>) as MessageModel;
+          if(e['type'] == 2)
+            return MessagePlaceModel.fromMap(e as Map<String, dynamic>) as MessageModel;
+
+          return MessageModel.fromMap(e as Map<String, dynamic>);
+        }).toList();
   }
 }
