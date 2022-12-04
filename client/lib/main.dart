e288cc800ff42d832b555b7292ecd19e69381686
Renan Rocha <renanrocha@MacBook-Air-de-Renan.local>
2022-12-04 15:05:20 -0300

    Configuração do firebase

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:sticker_swap_client/src/app.dart';
 import 'package:sticker_swap_client/src/app_module.dart';
 
-void main() async{
-  await dotenv.load(fileName: ".env");
+void main()async{
+  //await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(ModularApp(module: AppModule(), child: App(),));
 }
--- client/lib/src/modules/message_chat/infra/models/message_place_model.dart
+++ client/lib/src/modules/message_chat/infra/models/message_place_model.dart
@@ -1,17 +1,20 @@
 import 'package:sticker_swap_client/src/modules/message_chat/domain/entities/message_place.dart';
 
 class MessagePlaceModel extends MessagePlace {
-  MessagePlaceModel(
-      {required super.time,
-      required super.place,
-      required super.status,
-      required super.id,
-      required super.idSender});
+  MessagePlaceModel({
+    required super.time,
+    required super.place,
+    required super.status,
+    required String date,
+    required super.id,
+    required super.idSender
+  }) : super(date: date);
 
   factory MessagePlaceModel.fromMap(Map<String, dynamic> map) {
     return MessagePlaceModel(
       id: map['id'],
       time: map['time'],
+      date: map['date'],
       place: map['place'],
       status: map['status'],
       idSender: map['idSender'],
